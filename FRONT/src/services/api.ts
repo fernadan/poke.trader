11409618a056e65a71b8f555b21d648c794c9f51
Danilo Fernandes <danilo_fernandes@msn.com>
2021-02-12 12:23:11 -0300

    Alteração do caminho do serviço de API.

--- FRONT/src/services/api.ts
+++ FRONT/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:44317/api',
+  baseURL: 'https://aspnetdeploy-poke-trader.azurewebsites.net/api',
 });
 
 export default api;
